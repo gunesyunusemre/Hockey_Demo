d7cf304d5ea79379ef66f3d2509aa5aa4363d802
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-04-22 22:57:48 +0300

    +adding, refactoring: Ball color changer

--- Assets/Scripts/Player/ChangeBallColor.cs
+++ Assets/Scripts/Player/ChangeBallColor.cs
@@ -1,9 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using Ball;
+using Unity.Mathematics;
+using UnityEngine;
 
 namespace Player
 {
     public class ChangeBallColor : MonoBehaviour
     {
+        [SerializeField] private Color playerColor;
+
+        [SerializeField] private Transform leftPoint;
+        [SerializeField] private Transform rightPoint;
+
+        private IBall ball;
+        private Vector3 ballPosition;
+        private Vector3 ballLocalScale;
         
+        private void Update()
+        {
+            SetBall();
+
+            if (!CheckTrigger())
+                return;
+            
+            ball.ChangeColor(playerColor);
+        }
+        
+        private void SetBall()
+        {
+            ball = BallHelper.Ball;
+            ballPosition = ball.BallTransform.position;
+            ballLocalScale = ball.BallTransform.localScale;
+        }
+
+        private bool CheckTrigger()
+        {
+            if (math.abs(transform.position.y-ballPosition.y) > 
+                transform.localScale.y/2f+ballLocalScale.y/2f)
+                return false;
+
+            
+            if (ballPosition.x < leftPoint.position.x || ballPosition.x > rightPoint.position.x)
+                return false;
+
+            return true;
+        }
     }
 }
