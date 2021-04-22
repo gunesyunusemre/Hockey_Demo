fb41f5ab99aa8dd6486857bc0ceb5d7180d4bcc5
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-04-22 22:57:24 +0300

    +adding, refactoring: ball mono and Interface

--- Assets/Scripts/Ball/BallMono.cs
+++ Assets/Scripts/Ball/BallMono.cs
@@ -1,9 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
-namespace Test.Ball
+namespace Ball
 {
-    public class BallMono : MonoBehaviour
+    public class BallMono : MonoBehaviour, IBall
     {
-        
+        [SerializeField] private SpriteRenderer ballSprite;
+        private Vector3 dir;
+        public Transform BallTransform { get; private set; }
+
+        private void OnEnable()
+        {
+            BallTransform = transform;
+            this.InitializeBall();
+        }
+        private void OnDisable()
+        {
+            this.DestroyBall();
+        }
+
+        private void Start()
+        {
+            dir=Vector3.down;
+        }
+
+        private void Update()
+        {
+            transform.Translate(dir * (Time.deltaTime * 5f));
+        }
+
+        public void ChangeDirection(Vector3 newDir)
+        {
+            dir = newDir;
+        }
+
+        public void ChangeDirection(float value)
+        {
+            dir *= value;
+        }
+
+        public void ChangeColor(Color color)
+        {
+            ballSprite.color = color;
+        }
     }
 }
--- Assets/Scripts/Ball/IBall.cs
+++ Assets/Scripts/Ball/IBall.cs
@@ -1,7 +1,29 @@
-﻿namespace Ball
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ball
 {
+    public static class BallHelper
+    {
+        public static IBall Ball;
+
+        public static void InitializeBall(this IBall ball)
+        {
+            Ball = ball;
+        }
+
+        public static void DestroyBall(this IBall ball)
+        {
+            Ball = null;
+        }
+    }
+    
     public interface IBall
     {
-        
+        Transform BallTransform { get; }
+        void ChangeDirection(Vector3 newDir);
+        void ChangeDirection(float value);
+
+        void ChangeColor(Color color);
     }
 }
