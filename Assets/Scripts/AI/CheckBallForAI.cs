838bd7ad8da581baad79f6ccc1652c030d93f0c0
Yunus Emre GÜNEŞ <77919767+gunesyunusemre@users.noreply.github.com>
2021-04-23 11:10:12 +0300

    +adding: Check Ball Up and Down

---
+++ Assets/Scripts/AI/CheckBallForAI.cs
@@ -0,0 +1,30 @@
+﻿using Ball;
+using Manager;
+using Player;
+using UnityEngine;
+
+namespace AI
+{
+    public class CheckBallForAI : MonoBehaviour
+    {
+        private IBall _ball;
+        private Vector3 _ballPos;
+        
+        private void Update()
+        {
+            SetBall();
+
+            if (!(_ballPos.y > transform.position.y + transform.localScale.y / 2)) return;
+            
+            //Spawn Ball
+            _ball.ChangeType(PlayerType.PC);
+            SpawnManager.instance.SpawnBall();
+        }
+        
+        private void SetBall()
+        {
+            _ball = BallHelper.Ball;
+            _ballPos = _ball.BallTransform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/CheckBallForPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ball;
+using Manager;
+using UnityEngine;
+
+namespace Player
+{
+    public class CheckBallForPlayer : MonoBehaviour
+    {
+        private IBall _ball;
+        private Vector3 _ballPos;
+        private void Update()
+        {
+            SetBall();
+
+            if (!(_ballPos.y < transform.position.y - transform.localScale.y / 2)) return;
+            
+            //Spawn Ball
+            _ball.ChangeType(PlayerType.Player);
+            SpawnManager.instance.SpawnBall();
+        }
+
+        private void SetBall()
+        {
+            _ball = BallHelper.Ball;
+            _ballPos = _ball.BallTransform.position;
+        }
+    }
+}
